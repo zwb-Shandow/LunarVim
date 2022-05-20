d1d01f6c1d84df009a33d2b55eab5f58699903c3
zhangwenbo <zhangwenbo@trunk.tech>
2022-05-20 08:49:39 +0800

    新增 json/yaml/markdown 的格式化

--- utils/installer/config.example.lua
+++ utils/installer/config.example.lua
@@ -118,15 +118,15 @@ local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
   { command = "black", filetypes = { "python" } },
   -- { command = "isort", filetypes = { "python" } },
-  -- {
-  --   -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
-  --   command = "prettier",
-  --   ---@usage arguments to pass to the formatter
-  --   -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
-  --   extra_args = { "--print-with", "100" },
-  --   ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
-  --   filetypes = { "typescript", "typescriptreact" },
-  -- },
+  {
+    -- each formatter accepts a list of options identical to https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md#Configuration
+    command = "prettier",
+    ---@usage arguments to pass to the formatter
+    -- these cannot contain whitespaces, options such as `--line-width 80` become either `{'--line-width', '80'}` or `{'--line-width=80'}`
+    extra_args = { "--print-with", "100" },
+    ---@usage specify which filetypes to enable. By default a providers will attach to all the filetypes it supports.
+    filetypes = { "yaml", "json", "markdown" },
+  },
 }
 
 -- -- set additional linters
@@ -192,7 +192,10 @@ lvim.plugins = {
         },
       })
     end,
-  }
+  },
+  {
+    "p00f/nvim-ts-rainbow",
+  },
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
